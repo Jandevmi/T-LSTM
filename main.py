3d051c1e19019c30aa26033b9deb5511ea9ce1bd
Jan <jan@evenly.io>
2021-12-02 21:25:26 +0100

    Early stopping on F1

--- main.py
+++ main.py
@@ -15,9 +15,11 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
 import sys
+np.set_printoptions(threshold=sys.maxsize)
 import math
 
 from TLSTM import TLSTM
@@ -33,40 +35,38 @@ def convert_one_hot(label_list):
 
 
 def training(path, learning_rate, training_epochs, train_dropout_prob, hidden_dim, fc_dim, key, model_path):
-    path_string = path + '/data_train.pkl'
-    data_train_batches = pd.read_pickle(path_string)
 
-    path_string = path + '/elapsed_train.pkl'
-    elapsed_train_batches = pd.read_pickle(path_string)
+    data_train_batches = pd.read_pickle(path + '/data_train.pkl')
+    elapsed_train_batches = pd.read_pickle(path + '/elapsed_train.pkl')
+    labels_train_batches = pd.read_pickle(path + '/label_train.pkl')
 
-    path_string = path + '/label_train.pkl'
-    labels_train_batches = pd.read_pickle(path_string)
+    data_test_batches = pd.read_pickle(path + '/data_test.pkl')
+    elapsed_test_batches = pd.read_pickle(path + '/elapsed_test.pkl')
+    labels_test_batches = pd.read_pickle(path + '/label_test.pkl')
 
-    path_string = path + '/hidden_ind_train.pkl'
-    hidden_ind_train = pd.read_pickle(path_string)
-
-    number_train_batches = len(data_train_batches)
     print("Train data is loaded!")
-
+    number_train_batches = len(data_train_batches)
+    number_test_batches = len(data_test_batches)
     input_dim = data_train_batches[0].shape[2]
     output_dim = labels_train_batches[0].shape[1]
 
     lstm = TLSTM(input_dim, output_dim, hidden_dim, fc_dim, key)
 
     cross_entropy, y_pred, y, logits, labels = lstm.get_cost_acc()
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
+    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy)
 
     init = tf.compat.v1.global_variables_initializer()
     saver = tf.compat.v1.train.Saver()
 
     with tf.compat.v1.Session() as sess:
         sess.run(init)
-        best_cost = 100
+        best_f1 = 0
         best_sess = sess
         best_epoch = 0
         for epoch in range(training_epochs):  #
             # Loop over all batches
-            total_cost = 0
+            f1 = 0
+            total_cost = 100
             for i in range(number_train_batches):  #
                 # batch_xs is [number of patients x sequence length x input dimensionality]
                 batch_xs, batch_ys, batch_ts = data_train_batches[i], labels_train_batches[i], \
@@ -76,10 +76,44 @@ def training(path, learning_rate, training_epochs, train_dropout_prob, hidden_di
                                                    lstm.keep_prob: train_dropout_prob, lstm.time: batch_ts})
                 total_cost += loss
 
-            print('total_cost: ' + str(total_cost), end='')
-            if total_cost <= best_cost:
+            print("Loss: {:.3f}".format(total_cost), end='')
+
+            Y_pred = []
+            Y_true = []
+            Labels = []
+            Logits = []
+            for i in range(number_test_batches):  #
+                batch_xs, batch_ys, batch_ts = data_test_batches[i], labels_test_batches[i], \
+                                               elapsed_test_batches[i]
+                batch_ts = np.reshape(batch_ts, [batch_ts.shape[0], batch_ts.shape[2]])
+                c_train, y_pred_train, y_train, logits_train, labels_train = sess.run(
+                    lstm.get_cost_acc(),
+                    feed_dict={lstm.input: batch_xs, lstm.labels: batch_ys,
+                               lstm.keep_prob: train_dropout_prob, lstm.time: batch_ts})
+
+                if i > 0:
+                    Y_true = np.concatenate([Y_true, y_train], 0)
+                    Y_pred = np.concatenate([Y_pred, y_pred_train], 0)
+                    Labels = np.concatenate([Labels, labels_train], 0)
+                    Logits = np.concatenate([Logits, logits_train], 0)
+                else:
+                    Y_true = y_train
+                    Y_pred = y_pred_train
+                    Labels = labels_train
+                    Logits = logits_train
+                f1_batch = f1_score(Y_true, Y_pred)
+                f1 += f1_batch
+                print("Y_true")
+                print(Y_true)
+                print('Y_pred')
+                print(Y_pred)
+                print("F1: " + str(f1_batch))
+
+            f1 = f1 / number_train_batches
+            print(" F1: {:.3f}".format(f1), end='')
+            if f1 > best_f1:
                 print(' better epoch: ' + str(epoch))
-                best_cost = total_cost
+                best_f1 = f1
                 best_sess = sess
                 best_epoch = epoch
             else:
@@ -90,7 +124,7 @@ def training(path, learning_rate, training_epochs, train_dropout_prob, hidden_di
                 break
 
         print("Training is over!")
-        saver.save(sess, model_path)
+        saver.save(best_sess, model_path)
 
         Y_pred = []
         Y_true = []
@@ -119,6 +153,12 @@ def training(path, learning_rate, training_epochs, train_dropout_prob, hidden_di
         total_acc = accuracy_score(Y_true, Y_pred)
         total_auc = roc_auc_score(Labels, Logits, average='micro')
         total_auc_macro = roc_auc_score(Labels, Logits, average='macro')
+        f1 = f1_score(Y_true, Y_pred)
+        print("Y_true")
+        print(Y_true)
+        print('Y_pred')
+        print(Y_pred)
+        print("Train F1 = {:.3f}".format(f1))
         print("Train Accuracy = {:.3f}".format(total_acc))
         print("Train AUC = {:.3f}".format(total_auc))
         print("Train AUC Macro = {:.3f}".format(total_auc_macro))
